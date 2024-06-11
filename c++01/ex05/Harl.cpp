fcb2db233a1ef466ab94165b063b2c49ad9943a9
yudai-shimoma <cdcdasdll@icloud.com>
2024-06-12 00:13:14 +0900

    #3 c++01 ex05 課題要件を間違えていて、コードを修正

--- c++01/ex05/Harl.cpp
+++ c++01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:17:34 by yshimoma          #+#    #+#             */
-/*   Updated: 2024/06/11 23:57:07 by yshimoma         ###   ########.fr       */
+/*   Updated: 2024/06/12 00:10:07 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,8 @@ void Harl::complain(std::string leval) {
         }
     }
     if (i != 4) {
-        for (; i < LEVEL_NUM; i++) {
-            void (Harl::*funcPtr)() = _funcArray[i];
-            (this->*funcPtr)();
-        }
+        void (Harl::*funcPtr)() = _funcArray[i];
+        (this->*funcPtr)();
     } else {
         std::cout << "[ Probably complaining about insignificant problems ]"
                   << std::endl;
@@ -54,7 +52,6 @@ void Harl::debug() {
         << "[" << Harl::DEBUG << "]\n"
         << "I love having extra bacon for my "
            "7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
-        << std::endl
         << std::endl;
 }
 
@@ -63,7 +60,6 @@ void Harl::info() {
               << "I cannot believe adding extra bacon costs more money. You "
                  "didn’t put enough bacon in my burger! If you did, I wouldn’t "
                  "be asking for more!"
-              << std::endl
               << std::endl;
 }
 
@@ -72,13 +68,11 @@ void Harl::warning() {
               << "I think I deserve to have some extra bacon for free. I’ve "
                  "been coming for\n"
               << "years whereas you started working here since last month."
-              << std::endl
               << std::endl;
 }
 
 void Harl::error() {
     std::cout << "[" << Harl::ERROR << "]\n"
               << "This is unacceptable! I want to speak to the manager now."
-              << std::endl
               << std::endl;
 }
